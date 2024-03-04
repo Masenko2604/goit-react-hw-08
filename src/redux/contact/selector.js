1ac9b2a1972965295470dc374acc2d3ca1bc9bc2
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-03-04 09:22:01 +0100

    Update selector.js

--- src/redux/contact/selector.js
+++ src/redux/contact/selector.js
@@ -2,14 +2,19 @@ import { createSelector } from "@reduxjs/toolkit";
 export const selectDataContacts = (state) => state.contact.items;
 export const selectFilter = (state) => state.filter;
 export const selectIsLoading = (state) => state.contact.isLoading;
-export const filtData = createSelector(
-  [selectDataContacts, selectFilter],
-  (contacts, filter) => {
-    if (filter === "") {
-      return contacts.filter( => {
-  
-      });
-    }
-    
-  }
-);
+
+
+const initialState = "";
+
+const filterSlice = createSelector({
+  name: "filter",
+  initialState: initialState,
+  reducers: {
+    filterNumbers: (state, action) => {
+      return action.payload;
+    },
+  },
+});
+
+export const { filterNumbers } = filterSlice.actions;
+export const filterReducer = filterSlice.reducer;
