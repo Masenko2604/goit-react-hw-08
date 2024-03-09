02292bca9f8ee3abc33cb1b39ddba5c9eb3d3f7e
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-03-09 16:34:16 +0100

    Update RegisterPage.jsx

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,4 +1,4 @@
-import { RegisterForm } from '../../pages/RegisterPage/RegisterPage';
+import { RegisterForm } from '../../components/RegisterForm/RegisterForm';
 import { DocTitle } from '../../components/DocTitle';
 
 export default function RegisterPage() {
