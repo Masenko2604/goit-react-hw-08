bb8da313ade0f83b320dcce4e0c85ad11a4a35a2
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-03-09 15:47:43 +0100

    update

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,19 +1,38 @@
+// import { useDispatch } from 'react-redux';
+// import { logout } from '../../redux/auth/operations';
+// import { useAuth } from '../../hooks/useAuth';
+// import css from './UserMenu.module.css';
+// import { Button } from '@mui/material';
+
+// export const UserMenu = () => {
+//   const dispatch = useDispatch();
+//   const { user } = useAuth();
+
+//   return (
+//     <div className={css.wrapper}>
+//       <p className={css.username}>Welcome, {user.name}</p>
+//       <Button variant="contained" onClick={() => dispatch(logout())}>
+//         Logout
+//       </Button>
+//     </div>
+//   );
+// };
 import { useDispatch } from 'react-redux';
 import { logout } from '../../redux/auth/operations';
 import { useAuth } from '../../hooks/useAuth';
 import css from './UserMenu.module.css';
-import { Button } from '@mui/material';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
   const { user } = useAuth();
+  console.log({ user });
 
   return (
     <div className={css.wrapper}>
       <p className={css.username}>Welcome, {user.name}</p>
-      <Button variant="contained" onClick={() => dispatch(logout())}>
+      <button type="button" onClick={() => dispatch(logout())}>
         Logout
-      </Button>
+      </button>
     </div>
   );
 };
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,11 @@
-import { LogInForm } from '../../components/LoginForm/LoginForm';
+import { LoginForm } from '../../components/LoginForm/LoginForm';
 import { DocTitle } from '../../components/DocTitle';
 
 export default function LoginPage() {
   return (
     <>
       <DocTitle>Login</DocTitle>
-      <LogInForm />
+      <LoginForm />
     </>
   );
 }
