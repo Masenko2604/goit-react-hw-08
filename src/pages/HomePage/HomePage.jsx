1f4644b4048b5cb76cf6c672952e81c03091ca8f
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-03-11 12:06:26 +0100

    update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,9 +7,6 @@ export default function HomePage() {
     <>
       <DocTitle>Home</DocTitle>
       <div className="context">
-        {/* <div className={css.phone}>
-          <GiRotaryPhone size={150} />
-        </div> */}
         <p className={css.text}></p>
       </div>
       <>
