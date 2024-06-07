9210c3c6cebc815ced8f2dc10bfa23ff64f730ce
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-06-07 22:04:48 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -10,7 +10,7 @@ export default function HomePage() {
       </div>
       <>
         <div>
-          <h1>Welcome to phonebook!</h1>
+          <h1>Welcome to the phone book!</h1>
           <h2>Please log in to your account or register.</h2>
         </div>
       </>
